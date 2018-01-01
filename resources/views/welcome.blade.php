4ed0a90e1bf37089cd18b4594962a12f7cd9c188
samuume <zong0004@edu.eal.dk>
2018-01-01 16:25:52 +0100

    environment setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="col-md-12">
+        <h1>My New Portfolio</h1>
+      </div>
+    </div>
+  </div>
+@endsection
